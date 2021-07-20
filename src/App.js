c5720dfbdc6dffa5e84ae03780e45a99e4845563
Anomalous Alpha <blackcarrera@msn.com>
2021-07-19 18:40:54 -0600

    prepping Nav component to use useState hooks to render stuff

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import About from './components/About';
 import Nav from './components/Nav';
+import Gallery from './components/Gallery';
 import './App.css';
+
 
 function App() {
   return (
     <div>
       <Nav></Nav>
       <main>
+        <Gallery></Gallery>
         <About></About>
       </main>
 
