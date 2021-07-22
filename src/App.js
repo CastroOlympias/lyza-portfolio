2ea9f122bf406e1dd4d4b67df7e9328af5b4ba0c
Anomalous Alpha <blackcarrera@msn.com>
2021-07-21 18:00:01 -0600

    contact nav and gallery are now conditionally rendered

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Gallery from './components/Gallery';
 import ContactForm from './components/Contact';
 
 function App() {
+
   const [categories] = useState([
     {
       name: 'commercial',
@@ -17,18 +18,28 @@ function App() {
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
+  const [contactSelected, setContactSelected] = useState(false);
+
   return (
     <div>
       <Nav
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
       ></Nav>
       <main>
         <div>
-          <ContactForm></ContactForm>
-          <Gallery currentCategory={currentCategory}></Gallery>
-          <About></About>
+          {!contactSelected ? (
+            <>
+              <Gallery currentCategory={currentCategory}></Gallery>
+              <About></About>
+            </>
+          ) : (
+            <ContactForm></ContactForm>
+          )}
+
 
         </div>
       </main>
